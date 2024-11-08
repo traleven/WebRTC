fa685f38746325186bad4fd4755995a768a594f7
traleven <idipster@gmail.com>
2024-11-08 10:48:27 +0100

    Straightforward adding visionOS platform to the Package.json

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "WebRTC",
-    platforms: [.iOS(.v10), .macOS(.v10_11)],
+    platforms: [.iOS(.v10), .macOS(.v10_11), .visionOS(.v1)],
     products: [
         .library(
             name: "WebRTC",
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/stasel/WebRTC/releases/download/130.0.0/WebRTC-M130.xcframework.zip",
+            url: "https://github.com/traleven/WebRTC/releases/download/130.0.1/WebRTC-M130.xcframework.zip",
             checksum: "3b09833ae2b3aaa3755dc21f194dcba7564cf26de6af04876e2737c4927a8703"
         ),
     ]
